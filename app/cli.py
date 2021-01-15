2cbfbb8bfd272e0ce44d1a04d2756e366a3cbf78
Frixs <17912365+Frixs@users.noreply.github.com>
2021-01-15 19:36:07 +0100

    Input/output program parameters

--- app/cli.py
+++ app/cli.py
@@ -1,6 +1,8 @@
 # PLY's documentation: http://www.dabeaz.com/ply/ply.html
+import os
 from pprint import pprint, pp
 
+import sys
 import ply.lex
 import ply.yacc
 import app.lex
@@ -33,7 +35,19 @@ def main():
     # Build the lexical_analyzer
     lexer = ply.lex.lex(module=app.lex)
 
-    data = open('../inputs/calculator', 'r').read()
+    if len(sys.argv) < 3:
+        print("Parameters does not match the format!")
+        print("<input_code_file> <output_class_file>")
+        return 1
+
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
+
+    if not os.path.isfile(input_file):
+        print("Input file not accessible!")
+        return 1
+
+    data = open(input_file, 'r').read()
     lexer.input(data)
 
     # Tokenize
@@ -42,15 +56,15 @@ def main():
         if not tok:
             break  # No more input
         # print(tok)
-        print("{:<20} {:<30} {:<5} {:<5}".format(tok.type, tok.value, tok.lineno, tok.lexpos))
+        # print("{:<20} {:<30} {:<5} {:<5}".format(tok.type, tok.value, tok.lineno, tok.lexpos))
 
     parser = ply.yacc.yacc(module=app.syntax)
     ast = parser.parse(data, lexer=lexer, tracking=True)
 
     if app.sem.analyze(ast):
-        print_tree(ast)
+        # print_tree(ast)
         cls = generate("Main", ast)
-        f = open('Main.class', 'wb')
+        f = open(output_file, 'wb')
         create_classfile(cls, f)
         f.close()
 
